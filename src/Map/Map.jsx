ee8164242b5021323b11bd53b210d61b60c76abb
Pa6090 <pavanmittapalli1@gmail.com>
2024-04-28 15:22:51 +0530

    Initial Commit

---
+++ src/Map/Map.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
+import useStyles from './styles.js';
+import mapStyles from './mapStyles.js';
+
+const Map = ({setCoordinates, setBounds, coordinates, places, setChildClicked, weatherData}) => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+             bootstrapURLKeys={{key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY}}
+             defaultCenter={coordinates}
+             center={coordinates}
+             defaultZoom={14}
+             margin={[50,50,50,50]}
+             options={{ disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
+             onChange={(e)=> {
+                console.log(e)
+                setCoordinates({ lat: e.center.lat, lng: e.center.lng})
+                setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+             }}
+             onChildClick={(child) => setChildClicked(child)}
+            >
+                {places?.map((place, i)=>(
+                    <div className={classes.markerContainer} lat={Number(place.latitude)} lng={Number(place.longitude)} key={i} >
+                        {
+                            !isMobile ? (
+                                <LocationOnOutlinedIcon color='primary' fontSize='large' />
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography gutterBottom variant='subtitle2' className={classes.typography}>{place.name}</Typography>
+                                    <img className={classes.pointer} src={place.photo ? place.photo.images.large.url : 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%3Fk%3Drestaurant&psig=AOvVaw3wK1aP9uTgiyLoXLsnCBCw&ust=1714288892681000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJjDm_Ht4YUDFQAAAAAdAAAAABAE'} alt={place.name}/>
+                                    <Rating size='small' value={Number(place.rating)} readOnly/>
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
+                {weatherData?.list?.map((data, i)=> (
+                    <div key={i} lat={data.coord.lat} lng={data.coord.lng}>
+                        <img src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`} height={100}/>
+                    </div>
+                ))}
+            </GoogleMapReact>
+        </div>        
+    )
+}
+
+export default Map;
